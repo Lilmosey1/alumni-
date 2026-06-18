3e0acf53b36d7be8f9614da4c846b6d818d0981e
Lilmosey1 <stevekondejnr@gmail.com>
2026-06-18 20:29:15 +0300

    chore: update configuration and dependencies
    - Update README assets and project links
    - Link stylesheet and script to index.html
    - Update metadata schema
    - Fix icon import in Advertise page

--- pages/Advertise.tsx
+++ pages/Advertise.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Upload, CheckCircle, MapPin, kenyan shillingabbreviation, Tag } from 'lucide-react';
+import { Upload, CheckCircle, MapPin, DollarSign, Tag } from 'lucide-react';
 import { CATEGORIES, LOCATION_DATA } from '../constants';
 
 export const Advertise: React.FC = () => {
